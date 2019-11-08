9598d4b3853002d7e08a2652acedf68ba390ad6a
Cameron Zach <cameron.zach@temple.edu>
2019-11-08 16:42:24 -0500

    Fix port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 const {BattleRoom} = require('./room.js');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
