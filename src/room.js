1cb31075b2ffe94d72eeac806df4a4faa630d29a
Jonathan McFerran <jonathanmcferran@gmail.com>
2019-11-10 20:34:26 -0500

    Changed the behavior of the room to subtract damage from monsterHealth.

--- src/room.js
+++ src/room.js
@@ -3,6 +3,10 @@ const schema = require('@colyseus/schema');
 const Schema = schema.Schema;
 const MapSchema = schema.MapSchema;
 
+/**
+ * Defines State structure. The client-side representation must match this schema definition.
+ */
+
 class Player extends Schema {
 }
 schema.defineTypes(Player, {
@@ -28,6 +32,7 @@ class BattleRoom extends Room {
         this.battleName = options.battleName;
         this.playerMoves = {};
         this.maxClients = 10;
+        this.state.monsterHealth = 100; // TODO: add enemy starting health as a property in the options object.
         this.timer = this.clock.setInterval(() => this.doTurn(), 10000);
     }
 
@@ -36,16 +41,20 @@ class BattleRoom extends Room {
         let me = new Player();
         me.name = options.name;
         me.health = 100;
-        this.state.players[client.id] = me;
+        //this.state.players[client.id] = me;
     }
 
     onMessage (client, data) {
         console.log(client.id, "sent message");
+        //console.log(data);
         this.playerMoves[client.id] = data;
+        //console.log(this.playerMoves);
+
+        // Checks to see if all connected clients have sent a move to the server.
+        // If so, everyone has made a move and the current round can continue.
         if (this.clients.every(client => this.playerMoves[client.id] !== undefined)) {
-            this.doTurn();
-            this.playerMoves = {};
-            this.timer.reset();
+            console.log("Everyone has made their move!");
+            this.doTurn();            
         }
     }
 
@@ -57,10 +66,20 @@ class BattleRoom extends Room {
         console.log(`Disposing room ${this.battleName}`)
     }
 
+    /**
+     * At the end of every turn subtract the total damage given by all the connected clients
+     * from the enemie's current health. The state change will be reflected across all clients.
+     * Lastly, reset this round's player moves and reset the timer.
+     */
     doTurn() {
-        Object.keys(this.state.players).forEach(id => {
-            this.state.players[id].health -= 10;
+        var totalDamage = 0;
+        Object.keys(this.playerMoves).forEach(id => {            
+            totalDamage += parseFloat( this.playerMoves[id] );
         });
+        //console.log("Total Damage: " + totalDamage);
+        this.state.monsterHealth -= totalDamage;
+        this.playerMoves = {};
+        this.timer.reset();
     }
 }
 
