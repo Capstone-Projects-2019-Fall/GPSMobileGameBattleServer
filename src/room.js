11f26757482da8d987cfef8463fe75ff34c71bfb
Jonathan McFerran <jonathanmcferran@gmail.com>
2019-11-29 20:17:05 -0500

    Initialized enemy health from database, update enemy and node state after battle, added short circuit to end round early.

--- src/room.js
+++ src/room.js
@@ -1,5 +1,6 @@
 const {Room} = require("colyseus/lib/index");
 const schema = require('@colyseus/schema');
+const request = require('request');
 const Schema = schema.Schema;
 const MapSchema = schema.MapSchema;
 
@@ -25,6 +26,9 @@ schema.defineTypes(MyState, {
     monsterHealth: "number"
 });
 
+var baseURL = 'https://gps-mobile-game-server.herokuapp.com';
+// var baseURL = 'http://localhost:3000';
+
 class BattleRoom extends Room {
 
     onCreate(options) {
@@ -32,8 +36,8 @@ class BattleRoom extends Room {
         this.battleName = options.battleName;
         this.playerMoves = {};
         this.maxClients = 10;
-        this.state.monsterHealth = 100; // TODO: add enemy starting health as a property in the options object.
-        this.timer = this.clock.setInterval(() => this.doTurn(), 10000);
+        this.initializeEnemyHealth();
+        // this.timer = this.clock.setInterval(() => this.doTurn(), 10000);
     }
 
     onJoin (client, options) {
@@ -46,24 +50,55 @@ class BattleRoom extends Room {
 
     onMessage (client, data) {
         console.log(client.id, "sent message");
-        //console.log(data);
-        this.playerMoves[client.id] = data;
-        //console.log(this.playerMoves);
+        var move = JSON.parse(data);
+        // console.log(move);
+        this.playerMoves[client.id] = move;
+        console.log(this.playerMoves);
 
-        // Checks to see if all connected clients have sent a move to the server.
-        // If so, everyone has made a move and the current round can continue.
-        if (this.clients.every(client => this.playerMoves[client.id] !== undefined)) {
-            console.log("Everyone has made their move!");
-            this.doTurn();            
+        // If not all players have made their move AND "this" player's damage
+        // is enough to defeat the enemy, update the enemy's health to 0
+        // and let all other clients detect that the enemy has been defeated.
+        if(!this.checkPlayersMoves() && this.state.monsterHealth - move.damage <= 0){
+            this.state.monsterHealth = 0;
         }
     }
 
     onLeave (client) {
         console.log(client.id, "left");
+        delete this.state.players[client.id];
+
+        // We may need this for the case where there are multiple players in a room
+        // and everyone is waiting for the last player to make a move but gets disconnected.
+        // Since checkPlayersMoves() is only called in onMessage() the last client will not
+        // be able to send their move and the other players will wait indefinitely.
+        // Calling this method when a player leaves could solve this problem.
+        //this.checkPlayersMoves();
     }
 
     onDispose() {
-        console.log(`Disposing room ${this.battleName}`)
+        console.log("Remaining monster health: " + this.state.monsterHealth);
+        if(this.state.monsterHealth > 0)
+        {
+            this.updateEnemyHealth();
+        }
+        else
+        {
+            this.deleteEnemy();
+            this.updateNodeStructure();
+        }
+        
+        console.log(`Disposing room ${this.battleName}`);
+    }
+
+    checkPlayersMoves(){
+        // Checks to see if all connected clients have sent a move to the server.
+        // If so, everyone has made a move and the current round can continue.
+        if (this.clients.every(client => this.playerMoves[client.id] !== undefined)) {
+            console.log("Everyone has made their move!");
+            this.doTurn();
+            return true;     
+        }
+        return false;
     }
 
     /**
@@ -74,12 +109,101 @@ class BattleRoom extends Room {
     doTurn() {
         var totalDamage = 0;
         Object.keys(this.playerMoves).forEach(id => {            
-            totalDamage += parseFloat( this.playerMoves[id] );
+            totalDamage += this.playerMoves[id].damage;
         });
-        //console.log("Total Damage: " + totalDamage);
-        this.state.monsterHealth -= totalDamage;
+        
+        console.log("Total Damage: " + totalDamage);
+        if(this.state.monsterHealth - totalDamage <= 0){
+            this.state.monsterHealth = 0;
+        }
+        else{
+            this.state.monsterHealth -= totalDamage;
+        }
+
+        this.sendEnemyMove();
+
         this.playerMoves = {};
-        this.timer.reset();
+        // this.timer.reset();
+    }
+
+    sendEnemyMove()
+    {
+        // console.log("Sending enemy move...");
+        this.clients.every(client => this.send(client, "Punch 3 damage"));
+    }
+
+    initializeEnemyHealth()
+    {
+        request(baseURL + '/enemy/' + this.battleName , { json: true }, (err, res, body) => {
+            if (err)
+            { 
+                this.handleNetworkError(err);
+                this.state.monsterHealth = 100;
+            }
+            else if (res.statusCode != 200)
+            {
+                this.handleHTTPError(res, body);
+                this.state.monsterHealth = 100;
+            }
+            else
+            {
+                this.state.monsterHealth = body.hp;
+            }
+        });
+    }
+
+    updateEnemyHealth()
+    {
+        var postBody = {hp: this.state.monsterHealth};
+        request.post(baseURL + '/enemy/update/' + this.battleName , { body: postBody, json: true }, (err, res, body) => {
+            if (err)
+            { 
+                this.handleNetworkError(err);
+            }
+            else if (res.statusCode != 200)
+            {
+                this.handleHTTPError(res, body);
+            }
+        });
+    }
+
+    deleteEnemy()
+    {
+        request.delete(baseURL + '/enemy/' + this.battleName , (err, res, body) => {
+            if (err)
+            { 
+                this.handleNetworkError(err);
+            }
+            else if (res.statusCode != 200)
+            {
+                this.handleHTTPError(res, body);
+            }
+        });
+    }
+
+    updateNodeStructure()
+    {
+        var postBody = {structure: "Friendly"};
+        request.post(baseURL + '/geodata/update/' + this.battleName , { body: postBody, json: true }, (err, res, body) => {
+            if (err)
+            { 
+                this.handleNetworkError(err);
+            }
+            else if (res.statusCode != 200)
+            {
+                this.handleHTTPError(res, body);
+            }
+        });
+    }
+
+    handleNetworkError(err)
+    {
+        console.error(err); 
+    }
+
+    handleHTTPError(res, body)
+    {
+        console.error(res.statusCode + " " + res.statusMessage + ": " + body);
     }
 }
 
